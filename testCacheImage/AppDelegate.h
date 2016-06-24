d29d94e32910b79f99aae07a06c3b48995699dba
Evan <Evan@catskindemacbook-air-2.local>
2016-06-24 16:01:57 +0800

    Initial Commit

---
+++ testCacheImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testCacheImage
+//
+//  Created by Evan on 2016/6/24.
+//  Copyright © 2016年 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testCacheImage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  testCacheImage
+//
+//  Created by Evan on 2016/6/24.
+//  Copyright © 2016年 Evan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
